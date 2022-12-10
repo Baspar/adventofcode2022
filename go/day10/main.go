7de3d8b82e49261ae28eaf31e7bb9c2f2548d065
Baspar <me@bastien-laine.io>
2022-12-11 00:04:33 +0100

    Day10

--- go/day10/main.go
+++ go/day10/main.go
@@ -1,20 +1,86 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	utils "github.com/baspar/adventofcode2022/internal"
+	"github.com/baspar/adventofcode2022/internal/math"
 )
 
+type Instruction struct {
+	op       string
+	duration int
+	value    int
+}
+
 type DayImpl struct {
+	instructions []Instruction
+}
+
+func RunCPU(instructions []Instruction, callback func(cycle int, register int)) {
+	cycle, register := 1, 1
+
+	for _, instruction := range instructions {
+		execInstructionAtCycle := cycle + instruction.duration
+		for {
+			callback(cycle, register)
+
+			cycle++
+
+			if execInstructionAtCycle == cycle {
+				switch instruction.op {
+				case "addx":
+					register += instruction.value
+				}
+				break
+			}
+		}
+	}
 }
 
 func (d *DayImpl) Init(lines []string) error {
+	for _, line := range lines {
+		var instruction Instruction
+		if _, err := fmt.Sscanf(line, "addx %d", &instruction.value); err == nil {
+			instruction.op = "addx"
+			instruction.duration = 2
+		} else {
+			instruction.op = "noop"
+			instruction.duration = 1
+		}
+		d.instructions = append(d.instructions, instruction)
+	}
 	return nil
 }
 func (d *DayImpl) Part1() (string, error) {
-	return "", nil
+	strength := 0
+
+	RunCPU(d.instructions, func(cycle, register int) {
+		if (cycle-20)%40 == 0 {
+			strength += cycle * register
+		}
+	})
+
+	return fmt.Sprint(strength), nil
 }
 func (d *DayImpl) Part2() (string, error) {
-	return "", nil
+	screen := ""
+
+	RunCPU(d.instructions, func(cycle, spriteCenter int) {
+		pixel := (cycle - 1) % 40
+		if math.Abs(pixel-spriteCenter) < 2 {
+			screen += "▒"
+		} else {
+			screen += "."
+		}
+
+		if pixel == 39 {
+			screen += "\n"
+		}
+	})
+
+	return strings.TrimSuffix(screen, "\n"), nil
 }
 
 func main() {
--- go/day10/main_test.go
+++ go/day10/main_test.go
@@ -7,16 +7,171 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = ``
+var input = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`
+
+var output = `▒▒..▒▒..▒▒..▒▒..▒▒..▒▒..▒▒..▒▒..▒▒..▒▒..
+▒▒▒...▒▒▒...▒▒▒...▒▒▒...▒▒▒...▒▒▒...▒▒▒.
+▒▒▒▒....▒▒▒▒....▒▒▒▒....▒▒▒▒....▒▒▒▒....
+▒▒▒▒▒.....▒▒▒▒▒.....▒▒▒▒▒.....▒▒▒▒▒.....
+▒▒▒▒▒▒......▒▒▒▒▒▒......▒▒▒▒▒▒......▒▒▒▒
+▒▒▒▒▒▒▒.......▒▒▒▒▒▒▒.......▒▒▒▒▒▒▒.....`
 
 var inputs = map[string][2]string{
-	input: {"", ""},
+	input: {"13140", output},
 }
 
 func TestPart1(t *testing.T) {
 	assert := assert.New(t)
 
 	for input, expectedRes := range inputs {
+		if expectedRes[0] == "" {
+			continue
+		}
 		d := &DayImpl{}
 		d.Init(utils.SanitizeInput(input))
 
@@ -31,6 +186,9 @@ func TestPart2(t *testing.T) {
 	assert := assert.New(t)
 
 	for input, expectedRes := range inputs {
+		if expectedRes[1] == "" {
+			continue
+		}
 		d := &DayImpl{}
 		d.Init(utils.SanitizeInput(input))
 
